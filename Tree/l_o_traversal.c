a39c4d85eacb05758cc53b6a70e7f7307e463707
Sougata Pal <palsougata256@gmail.com>
2024-04-29 20:32:53 +0530

    modified:   Tree/l_o_traversal.c

--- Tree/l_o_traversal.c
+++ Tree/l_o_traversal.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+struct node
+{
 	int data;
 	struct node *left, *right;
 };
 
-void printCurrentLevel(struct node* root, int level);
-int height(struct node* node);
-struct node* newNode(int data);
+void printCurrentLevel(struct node *root, int level);
+int height(struct node *node);
+struct node *newNode(int data);
 
-void printLevelOrder(struct node* root)
+void printLevelOrder(struct node *root)
 {
 	int h = height(root);
 	int i;
@@ -19,25 +20,26 @@ void printLevelOrder(struct node* root)
 		printCurrentLevel(root, i);
 }
 
-void printCurrentLevel(struct node* root, int level)
+void printCurrentLevel(struct node *root, int level)
 {
 	if (root == NULL)
 		return;
 	if (level == 1)
 		printf("%d ", root->data);
-	else if (level > 1) {
+	else if (level > 1)
+	{
 		printCurrentLevel(root->left, level - 1);
 		printCurrentLevel(root->right, level - 1);
 	}
 }
 
-
-int height(struct node* node)
+int height(struct node *node)
 {
 	if (node == NULL)
 		return 0;
-	else {
-		
+	else
+	{
+
 		int lheight = height(node->left);
 		int rheight = height(node->right);
 
@@ -48,11 +50,46 @@ int height(struct node* node)
 	}
 }
 
-
-struct node* newNode(int data)
+void breadthFirstTraversal(struct node *root)
 {
-	struct node* node
-		= (struct node*)malloc(sizeof(struct node));
+	if (root == NULL)
+		return;
+	struct node *queue[100];
+	int front = 0, rear = 0;
+	queue[rear++] = root;
+	while (front < rear)
+	{
+		struct node *current = queue[front++];
+		printf("%d ", current->data);
+		if (current->left != NULL)
+			queue[rear++] = current->left;
+		if (current->right != NULL)
+			queue[rear++] = current->right;
+	}
+}
+
+// void bfs(struct node *root){
+// 	if(root ==NULL){
+// 		return;
+// 	}
+// 	struct node *queue[100];
+// 	int front =0,rear=0;
+// 	queue[rear++]=root;
+// 	while(front<rear){
+// 		struct node * current=queue[front++];
+// 		printf("%d ",current->data);
+// 		if(current->left!=NULL){
+// 			queue[rear++]=current->left;
+// 		}
+// 		if(current->right!=NULL){
+// 			queue[rear++]=current->right;
+// 		}
+// 	}
+// }
+
+struct node *newNode(int data)
+{
+	struct node *node = (struct node *)malloc(sizeof(struct node));
 	node->data = data;
 	node->left = NULL;
 	node->right = NULL;
@@ -62,14 +99,16 @@ struct node* newNode(int data)
 
 int main()
 {
-	struct node* root = newNode(1);
-	root->left = newNode(2);
+	struct node *root = newNode(1);
+	root->left = newNode(7);
 	root->right = newNode(3);
 	root->left->left = newNode(4);
-	root->left->right = newNode(5);
+	root->left->right = newNode(3);
 
 	printf("Level Order traversal of binary tree is \n");
 	printLevelOrder(root);
+	printf("\n\n");
+	bfs(root);
 
 	return 0;
 }
