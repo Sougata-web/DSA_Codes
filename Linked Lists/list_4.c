4d1de4bd3f4be6bd6bc3136f2d64e37ceae14499
Sougata Pal <palsougata256@gmail.com>
2024-05-09 20:14:36 +0530

    modified:   Linked Lists/list_10.c 	modified:   Linked Lists/list_11.c 	modified:   Linked Lists/list_2.c 	modified:   Linked Lists/list_3.c 	modified:   Linked Lists/list_4.c 	modified:   Linked Lists/list_5.c 	modified:   Linked Lists/list_6.c 	modified:   Linked Lists/list_7.c 	modified:   Linked Lists/list_8.c

--- Linked Lists/list_4.c
+++ Linked Lists/list_4.c
@@ -1,42 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct node
 {
     int data;
     struct node *next;
 };
-struct node *start=NULL;
- struct node *m;
+struct node *start = NULL;
+struct node *m;
 
-    struct node *temp;
-void insert(int value){
-    temp=(struct node*)malloc(sizeof(struct node));
-    temp->data=value;
-    temp->next=NULL;
-    if(start==NULL){
-        start=temp;
+struct node *temp;
+void insert(int value)
+{
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = value;
+    temp->next = NULL;
+    if (start == NULL)
+    {
+        start = temp;
     }
-    else{
+    else
+    {
         struct node *m;
-        m=start;
-        while(m->next != NULL)
-            m=m->next;
-        m->next=temp;
+        m = start;
+        while (m->next != NULL)
+            m = m->next;
+        m->next = temp;
     }
 }
 
-int printList(){
-    m=start;
+int printList()
+{
+    m = start;
     printf("Printing linked lists values:\n");
-    while(m!=NULL){
-        printf("%d ",m->data);
-    m=m->next;
+    while (m != NULL)
+    {
+        printf("%d ", m->data);
+        m = m->next;
     }
 }
 
-void del_begin(){
-    temp=start;
-    start=start->next;
+void del_begin()
+{
+    temp = start;
+    start = start->next;
     free(temp);
 }
 
@@ -54,24 +60,29 @@ void del_begin(){
 //     temp->next=NULL;
 // }
 
-int main(){
-    int i,n,x,ch2;
+int main()
+{
+    int i, n, x, ch2;
     char ch;
     printf("Enter the number of values you want to store:\n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("Enter the values you want to store:\n");
-    for(i=0;i<n;i++){
-        scanf("%d",&x);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
         insert(x);
     }
     printList();
     printf("\nDo you want to delete first node?");
     printf("\nIf yes enter Y and if no enter N..\n");
-    scanf("%c",&ch);
-    scanf("%c",&ch);
-    if(ch=='Y'||ch=='y'){
+    scanf("%c", &ch);
+    scanf("%c", &ch);
+    if (ch == 'Y' || ch == 'y')
+    {
         del_begin();
-    }else if(ch=='N'|ch=='n'){
+    }
+    else if (ch == 'N' | ch == 'n')
+    {
         printf("First node is not deleted\n");
         // printf("\nDo you want to delete last node?");
         // printf("\nIf yes enter 1 and if no enter 0..\n");
@@ -82,9 +93,9 @@ int main(){
         //     printf("Node is not deleted");
         // }else{
         // printf("Wrong input");
-
-        }
-    else{
+    }
+    else
+    {
         printf("Wrong input");
     }
     printList();
--- Linked Lists/list_6.c
+++ Linked Lists/list_6.c
@@ -1,55 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 typedef struct node
 {
     int data;
     struct node *next;
-}node;
+} node;
 node *l;
- node *insert_end(int data,node*start){
+node *insert_end(int data, node *start)
+{
     node *newnode;
-    newnode=(node*)malloc(sizeof(node));
-    newnode->data=data;
-    newnode->next=NULL;
+    newnode = (node *)malloc(sizeof(node));
+    newnode->data = data;
+    newnode->next = NULL;
 
-    if(start==NULL){
-        start=newnode;
-
-    }else{
+    if (start == NULL)
+    {
+        start = newnode;
+    }
+    else
+    {
         node *m;
-        m=start;
-        while(m->next!=NULL){
-            m=m->next;
+        m = start;
+        while (m->next != NULL)
+        {
+            m = m->next;
         }
-        m->next=newnode;
+        m->next = newnode;
     }
     return start;
- }
+}
 
-
-int main(){
-    int i,n,x,y,z=1;
-    node*link1=NULL;
-    node*link2=NULL;
-    node*last=NULL;
+int main()
+{
+    int i, n, x, y, z = 1;
+    node *link1 = NULL;
+    node *link2 = NULL;
+    node *last = NULL;
     printf("Enter the size of linked list:\n");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        printf("Enter the first linked lists %dno value at position %d:\n",z,i);
-        scanf("%d",&x);
-        link1=insert_end(x,link1);
-        printf("Enter the second linked lists %dno value at position %d:\n",z,i);
-        scanf("%d",&y);
-        link2=insert_end(y,link2);
-        last=insert_end(x+y,last);
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        printf("Enter the first linked lists %dno value at position %d:\n", z, i);
+        scanf("%d", &x);
+        link1 = insert_end(x, link1);
+        printf("Enter the second linked lists %dno value at position %d:\n", z, i);
+        scanf("%d", &y);
+        link2 = insert_end(y, link2);
+        last = insert_end(x + y, last);
         z++;
     }
-    l=last;
+    l = last;
     printf("Printing linked lists values:\n");
-    while(l!=NULL){
-        printf("%d ",l->data);
-    l=l->next;
+    while (l != NULL)
+    {
+        printf("%d ", l->data);
+        l = l->next;
     }
-    
+
     return 0;
 }
