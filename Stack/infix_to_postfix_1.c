375b77346de851b0f074913382abc9b40318dcc2
Sougata Pal <palsougata256@gmail.com>
2024-05-14 20:20:36 +0530

    modified:   Stack/infix_to_postfix_1.c

--- Stack/infix_to_postfix_1.c
+++ Stack/infix_to_postfix_1.c
@@ -1,74 +1,83 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define MAX_NUMBER_CHAR 100
 
-int precedence(char c){
-    if(c=='^')
+int precedence(char c)
+{
+    if (c == '^')
         return 3;
-    else if(c=='/' || c=='*')
+    else if (c == '/' || c == '*')
         return 2;
-    else if(c=='+' || c=='-')
+    else if (c == '+' || c == '-')
         return 1;
     else
         return -1;
 }
 
-char associativity_rule(char c){
-    if(c=='^')
+char associativity_rule(char c)
+{
+    if (c == '^')
         return 'R';
     return 'L';
 }
 
-void infix_to_postfix(char s[]){
+void infix_to_postfix(char s[])
+{
     char stack[MAX_NUMBER_CHAR];
-    int result_index=0;
+    int result_index = 0;
     char result[MAX_NUMBER_CHAR];
-    int len= strlen(s);
-    int stack_index=-1;
+    int len = strlen(s);
+    int stack_index = -1;
 
-    for(int i=0;i<len;i++){
-        char c= s[i];
-        if((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='1'&& c<='9')){
-            result[result_index++]=c;
+    for (int i = 0; i < len; i++)
+    {
+        char c = s[i];
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '1' && c <= '9'))
+        {
+            result[result_index++] = c;
         }
-        else if(c== '('){
-            stack[++stack_index]=c;
+        else if (c == '(')
+        {
+            stack[++stack_index] = c;
         }
-        else if(c==')'){
-            while(stack_index>=0 && stack[stack_index] != ')'){
-                result[result_index++]=stack[stack_index--];
-
+        else if (c == ')')
+        {
+            while (stack_index >= 0 && stack[stack_index] != ')')
+            {
+                result[result_index++] = stack[stack_index--];
             }
             stack_index--;
         }
-        else{
-            while(stack_index>=0 && ((precedence(s[i])<precedence(stack[stack_index])||(precedence(s[i])==precedence(stack[stack_index]))&&associativity_rule(s[i])=='L'))){
-                result[result_index++]=stack[stack_index--];
+        else
+        {
+            while (stack_index >= 0 && ((precedence(s[i]) < precedence(stack[stack_index]) || (precedence(s[i]) == precedence(stack[stack_index])) && associativity_rule(s[i]) == 'L')))
+            {
+                result[result_index++] = stack[stack_index--];
             }
-            stack[++stack_index]=c;
+            stack[++stack_index] = c;
         }
     }
-    while(stack_index>=0){
-        result[result_index++]=stack[stack_index--];
-
+    while (stack_index >= 0)
+    {
+        result[result_index++] = stack[stack_index--];
     }
-    result[result_index]=='\0';
-    printf("%s\n",result);
-
+    result[result_index] == '\0';
+    printf("%s\n", result);
 }
 
-void infix_to_prefix(char s[]){
-    
+void infix_to_prefix(char s[])
+{
 }
-int main(){
-   
+int main()
+{
+
     char exp[100];
-    
+
     printf("Enter the expression: \n");
-    scanf("%[^\n]s",exp);
+    scanf("%[^\n]s", exp);
 
     infix_to_postfix(exp);
-    
+
     return 0;
 }
