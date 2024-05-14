7b95971fb2a0d49789ab8529b55321f123b3939c
Sougata Pal <palsougata256@gmail.com>
2024-05-14 20:21:24 +0530

    modified:   Array/arr_sort_merge.c 	modified:   Array/column_major.c

--- Array/arr_sort_merge.c
+++ Array/arr_sort_merge.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 int main()
 {
     int i = 0, j = 0, k = 0, m, n;
@@ -54,7 +54,6 @@ int main()
         j++;
         k++;
     }
-
 
     printf("The sorted array:\n");
     for (i = 0; i < m + n; i++)
--- Array/column_major.c
+++ Array/column_major.c
@@ -1,19 +1,24 @@
-#include<stdio.h>
-int main(){
-    int c=5,r=6,i,j,a[r][c],k;
-    for(i=0;i<c;i++){
-        for(j=0;j<r;j++){
-            a[j][i]=(i*j+1);
+#include <stdio.h>
+int main()
+{
+    int c = 5, r = 6, i, j, a[r][c], k;
+    for (i = 0; i < c; i++)
+    {
+        for (j = 0; j < r; j++)
+        {
+            a[j][i] = (i * j + 1);
         }
     }
 
-    for(i=0;i<c;i++){
-        for(j=0;j<r;j++){
-            printf("%d ",a[j][i]);
+    for (i = 0; i < c; i++)
+    {
+        for (j = 0; j < r; j++)
+        {
+            printf("%d ", a[j][i]);
         }
         printf("\n");
     }
-        int *base=&a[0][0];
-    printf("%d",base+((i*5)+j)*sizeof(int));
+    int *base = &a[0][0];
+    printf("%d", base + ((i * 5) + j) * sizeof(int));
     return 0;
 }
