bf4d5c56150ccdf80aecc78093bedf5615ee545d
Sougata Pal <palsougata256@gmail.com>
2024-05-25 22:23:03 +0530

    modified:   LeetCode/202_happy_number.c 	modified:   LeetCode/26_remove_dupli_sorted_array.c

--- LeetCode/26_remove_dupli_sorted_array.c
+++ LeetCode/26_remove_dupli_sorted_array.c
@@ -1,20 +1,22 @@
-#include<stdio.h>
-int main(){
-    int i,k,n;
+#include <stdio.h>
+int main()
+{
+    int i, k, n;
     printf("Enter the number of element you want to store in array:\n");
-    scanf("%d",&n);
-    int nums[n],expectedNumber[100];
+    scanf("%d", &n);
+    int nums[n], expectedNumber[100];
     printf("Enter the array elements:\n");
-    for(i=0;i<n;i++){
-        scanf("%d",&nums[i]);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &nums[i]);
     }
 
-    while(i<n){
-        if (nums[i]==nums[i+1])
+    while (i < n)
+    {
+        if (nums[i] == nums[i + 1])
         {
             /* code */
         }
-        
     }
     return 0;
 }
